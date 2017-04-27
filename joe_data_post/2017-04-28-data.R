69881c2f269e50a7af4fb5668843ed667f2982a9
Joe Brew <joebrew@gmail.com>
2017-04-27 18:32:25 +0200

    new post

---
+++ joe_data_post/2017-04-28-data.R
@@ -0,0 +1,115 @@
+library(tidyverse)
+library(cism)
+library(ggthemes)
+
+wb <- cism::wb
+indicators <- data_frame(indicator = sort(unique(wb$indicator_name)))
+
+wb_spain <- read_csv('data/API_ESP_DS2_en_csv_v2.csv', skip = 4)
+wb_spain <- wb_spain[,1:61]
+names(wb_spain)[5:ncol(wb_spain)] <- paste0('y',
+                                names(wb_spain)[5:ncol(wb_spain)])
+
+# Make long
+wb_spain <- gather(wb_spain,
+             year,
+             value,
+             y1960:y2016)
+
+# Clean up
+wb_spain$year <- as.numeric(gsub('y', '', wb_spain$year))
+
+# Rename
+names(wb_spain) <- c('country',
+               'country_code',
+               'indicator_name',
+               'indicator_code',
+               'year',
+               'value')
+
+# Don't save empty stuff
+wb_spain <-
+  wb_spain %>%
+  group_by(indicator_code) %>%
+  mutate(flag = length(which(!is.na(value))) == 0) %>%
+  ungroup %>%
+  filter(!flag) %>%
+  dplyr::select(-flag)
+
+
+indices <- c(indices <- c(3,4,5,11, 12,19, 46, 53, 84, 94, 113, 151, 168, 170, 210, 217, 218, 443, 448, 453, 482, 530, 532, 533, 584, 587, 588, 589, 591, 593, 637, 642,  674, 675, 680, 681, 695, 698, 696, 742,  746, 745, 752, 756, 894, 896, 919, 924, 936, 940, 941, 1020, 1026, 1027, 1035, 1069, 1075: 1080, 1158, 1159, 1176, 1177, 1216:1226, 1297, 1301))
+indicators <- indicators[indices,]
+
+# Combine, keeping only those indicators we're interested
+df <- wb %>% 
+  bind_rows(wb_spain) %>%
+  filter(indicator_name %in% indicators$indicator)
+
+# Define funtion for plotting
+joe_plot <-
+  function(indicator){
+    x <- df %>%
+      filter(indicator_name == indicator) %>%
+      filter(!is.na(value))
+    
+    has_spain <- 'ESP' %in% x$country_code
+    title_size <- ifelse(nchar(indicator) > 70,
+                         6,
+                         ifelse(nchar(indicator) > 55,
+                                8,
+                         ifelse(nchar(indicator) > 40,
+                                11,
+                                13)))
+    
+    if(has_spain){
+      g <- ggplot(data = x,
+                  aes(x = year,
+                      y = value,
+                      group = country,
+                      color = country)) 
+    } else {
+      g <- ggplot(data = x,
+                  aes(x = year,
+                      y = value))
+    }
+    g <- g +
+      geom_point(alpha = 0.7) +
+      geom_line(alpha = 0.5) +
+      theme_fivethirtyeight() +
+      ggtitle(x$indicator_name) +
+      theme(plot.title = element_text(family = "Trebuchet MS", 
+                                      color="#666666", 
+                                      face="bold", 
+                                      size=title_size, 
+                                      hjust=0)) +
+      scale_color_manual(name = 'Country',
+                         values = c('darkred', 'darkblue'))
+      
+    return(g)
+  }
+
+options(scipen = '999')
+for (i in 1:nrow(indicators)){
+
+  try({
+    the_indicator <- indicators$indicator[i]
+    the_plot <- joe_plot(the_indicator)
+    print(the_plot)
+    ggsave(filename = paste0('../img/data/',
+                             the_indicator, 
+                             '.png'))
+  })
+}
+
+
+plots <- dir('../img/data')
+
+for (i in 1:length(plots)){
+  the_text <- paste0('<a href="/img/data/',
+                     plots[i],
+                     '"> <img border="0" src= "/img/data/',
+                     plots[i],
+                     '" width="200"></a>',
+                     '\n\n')
+  cat(the_text)
+}
